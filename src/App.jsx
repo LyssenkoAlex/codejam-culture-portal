9d52a1c6fdd59e699b87f5d941bf65c0bd509504
Nij4life <nij4life@gmail.com>
2020-02-21 17:27:12 +0300

    feat: add Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import Directors from "./components/Directors/Directors.jsx";
+import Directors from './components/Directors/Directors.jsx';
+import Header from './components/Header/Header.jsx';
 
 
 
 function App() {
   return (
     <React.Fragment>
-    <Directors />
+      <Header />
+      <Directors />
     </React.Fragment>
   );
 }
