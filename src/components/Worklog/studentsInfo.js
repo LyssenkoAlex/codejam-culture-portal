0c9591ad886f3c32d5f0a9125b488809994325e3
Karyuhin Sergej <karyuhin.sergej@gmail.com>
2020-02-22 21:39:34 +0300

    refactor: structure Worklog

---
+++ src/components/Worklog/studentsInfo.js
@@ -0,0 +1,169 @@
+export const studentsInfo = [
+  {
+    id: 1,
+    student: 'Alexandr Lyssenko',
+    task: 'Test',
+    time: '24 hours',
+  },
+  {
+    id: 2,
+    student: 'Himzheuski Vitali',
+    task: 'Test',
+    time: '24 hours',
+  },
+  {
+    id: 3,
+    student: 'Alexey Kostsevich',
+    task: 'Test',
+    time: '24 hours',
+  },
+  {
+    id: 4,
+    student: 'Aleksandr Serikov',
+    task: 'Test',
+    time: '24 hours',
+  },
+  {
+    id: 5,
+    student: 'Stanislav Iosifov',
+    task: 'Test',
+    time: '24 hours',
+  },
+  {
+    id: 6,
+    student: 'Siarhei Karukhin',
+    task: 'Test',
+    time: '24 hours',
+  }
+]
+
+
+
+export const MIN = [
+  {
+    id: 1,
+    checked: '1',
+    score: '10',
+    description: 'Main page + page with a list of authors + author`s page (only pages with content without widgets)',
+  },
+  {
+    id: 2,
+    checked: '0',
+    score: '10',
+    description: 'Page with team members + page with worklog',
+  },
+  {
+    id: 3,
+    checked: '1',
+    score: '10',
+    description: 'Page with list of authors contains search widget',
+  },
+  {
+    id: 4,
+    checked: '0',
+    score: '20',
+    description: 'Portal has two languages',
+  }
+]
+
+export const NORMAL = [
+  {
+    id: 1,
+    checked: '1',
+    score: '20',
+    description: 'Portal has page with styleguide',
+  },
+  {
+    id: 2,
+    checked: '0',
+    score: '10',
+    description: 'Mobile version is okey',
+  },
+  {
+    id: 3,
+    checked: '1',
+    score: '10',
+    description: 'Ipad/tablet version is okey',
+  },
+  {
+    id: 4,
+    checked: '0',
+    score: '10',
+    description: 'Authors page contains timeline',
+  },
+  {
+    id: 5,
+    checked: '1',
+    score: '10',
+    description: 'Authors page contains video overlay',
+  },
+  {
+    id: 6,
+    checked: '0',
+    score: '20',
+    description: 'Authors page contains photo gallery',
+  },
+  {
+    id: 7,
+    checked: '1',
+    score: '10',
+    description: 'Authors page contains map (geowidget)',
+  },
+  {
+    id: 8,
+    checked: '0',
+    score: '20',
+    description: 'Design (typography, icons, colors, links + buttons + input are styled)',
+  },
+  {
+    id: 9,
+    checked: '1',
+    score: '20',
+    description: 'Material-ui / bootstrap is used',
+  },
+  {
+    id: 10,
+    checked: '0',
+    score: '10',
+    description: 'Portal has third language',
+  }
+]
+
+export const EXTRA = [
+  {
+    id: 1,
+    checked: '1',
+    score: '10',
+    description: 'Confidence of the project presentation',
+  },
+  {
+    id: 2,
+    checked: '0',
+    score: '10',
+    description: 'Project is made using gatsbyjs',
+  },
+  {
+    id: 3,
+    checked: '1',
+    score: '10',
+    description: 'Contentful / netlify cms is used for content management',
+  },
+  {
+    id: 4,
+    checked: '0',
+    score: '20',
+    description: 'Animations / special effects like paralax',
+  },
+  {
+    id: 5,
+    checked: '1',
+    score: '20',
+    description: 'Outstanding design',
+  },
+  {
+    id: 6,
+    checked: '0',
+    score: '20',
+    description: 'Storybook/styleguidist/other react styleguide tool usage for the page with styles',
+  }
+]
