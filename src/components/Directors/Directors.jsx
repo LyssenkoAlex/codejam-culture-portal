1fb79d381e0b1554cd43400df033c086b143ab7f
AlexK <Green242526@gmail.com>
2020-02-24 01:35:25 +0300

    director search and css completed

--- src/components/Directors/Directors.jsx
+++ src/components/Directors/Directors.jsx
@@ -1,40 +1,62 @@
-import React, {Component} from 'react';
+import React, {useState} from 'react';
 import {connect} from 'react-redux';
 import PropTypes from 'prop-types';
+import './Directors.css';
 import {Link} from 'react-router-dom'
-import './Directors.scss';
 import {showDirector} from '../../redux/actions/actions';
+import {HOME} from '../../utils/utils';
+//import {Route} from "react-router-dom";
 
+function Directors(props) {
 
-class Directors extends Component {
-    render() {
-        const directors = this.props.directors.map(item => (
-            <img key={item.id} src={item.photo}>
-            </img>
-        ));
+  let [value, setValue] = useState('')
 
-        return (
-            <nav className='container_dir'>
-                    {directors}
-            </nav>
-        )
+  const searchDirector = (e) => {
+    if (e.currentTarget.value) {
+      let newValue = e.currentTarget.value
+      setValue(() => newValue.toLowerCase())
+    } else {
+      setValue(() => e.currentTarget.value)
     }
+  }
+
+  let authors = value ? props.directors.filter(d => (d.name[props.language].toLowerCase()).includes(value)) : props.directors
+
+  let elements = authors.map((item, index) =>
+
+    <li key={index} className = 'block_director'>
+      <Link to={HOME.path}>
+        {() => props.showDirector(item.id)}
+        <img src={item.photo}/>
+        <p>{item.name[props.language]}</p>
+      </Link>
+    </li>
+  )
+
+  return (
+    <div className = 'main_directors'>
+      <input onChange={searchDirector}/>
+      <ul className = 'block_directors'>
+        {elements}
+      </ul>
+    </div>
+  )
 }
 
 const mapDispatchToProps = {
-    showDirector
+  showDirector
 };
 
 
 const mapStateToProps = state => ({
-    directors: state.directors,
-    language: state.language
+  directors: state.directors,
+  language: state.language
 });
 export default connect(mapStateToProps, mapDispatchToProps)(Directors);
 
 Directors.propTypes = {
-    directors: PropTypes.arrayOf(PropTypes.any).isRequired,
-    language: PropTypes.string.isRequired,
-    showDirector: PropTypes.func.isRequired
+  directors: PropTypes.arrayOf(PropTypes.any).isRequired,
+  language: PropTypes.string.isRequired,
+  showDirector: PropTypes.func.isRequired
 };
 
