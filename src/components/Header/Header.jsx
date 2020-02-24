5b6ff775f5f552b87f4b02953d6255e1bc135031
Nij4life <nij4life@gmail.com>
2020-02-25 01:44:15 +0300

    feat: add select language

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,61 +1,63 @@
-import React, {Component} from 'react';
-import {LANG, NAV_PAGES, THEATER_TITLE} from '../../utils/utils';
-import {Link} from 'react-router-dom'
+import React, { Component } from 'react';
+import { LANG, NAV_PAGES } from '../../utils/utils';
+import { Link } from 'react-router-dom'
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import {changeLanguage} from '../../redux/actions/actions';
-
+import { changeLanguage } from '../../redux/actions/actions';
 
 import './Header.scss';
 
-class  Header extends Component {
-    render()
-    {
-        const articles = NAV_PAGES.map(article => (
-            <li key={article.ID}>
-                <Link to={article.path}>{article[this.props.language]}</Link>
-            </li>
-        ));
+class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.select = React.createRef();
+    this.onChangeLanguage = this.onChangeLanguage.bind(this);
+  }
 
-        let languages = [];
-        Object.keys(LANG).forEach((x) => languages.push(x));
-        languages = languages.map(langCode => (
-            <li key={LANG[langCode].CODE} onClick={() => this.props.changeLanguage(langCode)}>
-                <span>{LANG[langCode].TITLE}</span>
-            </li>
-        ));
+  onChangeLanguage() {
+    const lang = this.select.current.value.toUpperCase();
+    const {changeLanguage} = this.props;
+    changeLanguage( lang === 'EN' ? 'ENG' : lang);
+  }
 
+  render() {
+    const {language} = this.props;
+    const articles = NAV_PAGES.map(article => (
+      <li key={article.ID}>
+        <Link to={article.path}>{article[language]}</Link>
+      </li>
+    ));
 
-        return (
-            <header className="header">
-                <div className="header__top">
-                    <h1>{THEATER_TITLE[this.props.language]}</h1>
-                    <ul>
-                        {languages}
-                    </ul>
-                </div>
-                <nav className="header__nav">
-                    <ul>
-                        {articles}
-                    </ul>
-                </nav>
-            </header>
-        )
-    }
+    let languages = [];
+    Object.keys(LANG).forEach((x) => languages.push(x));
+
+    return (
+      <header className="header">
+        <nav className="header__nav">
+          <ul>
+            {articles}
+          </ul>
+        </nav>
+        <select className="lang" onChange={this.onChangeLanguage} ref={this.select}>
+          {languages.map(langCode=><option key={LANG[langCode].CODE}>{LANG[langCode].CODE}</option>)}
+        </select>
+      </header>
+    )
+  }
 }
 
 const mapDispatchToProps = {
-    changeLanguage
+  changeLanguage
 };
 
 const mapStateToProps = state => ({
-    language: state.language
+  language: state.language
 });
 
 
 export default connect(mapStateToProps, mapDispatchToProps)(Header);
 
 Header.propTypes = {
-    language: PropTypes.string.isRequired,
-    changeLanguage: PropTypes.func.isRequired
+  language: PropTypes.string.isRequired,
+  changeLanguage: PropTypes.func.isRequired
 };
