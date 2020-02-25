83d945c90d849957abee28284524783b4258987a
Karyuhin Sergej <karyuhin.sergej@gmail.com>
2020-02-25 16:38:30 +0300

    feat: add test images

--- src/components/MainPage/MainPage.jsx
+++ src/components/MainPage/MainPage.jsx
@@ -1,19 +1,23 @@
 import React from 'react';
 import './MainPage.css';
 
+import slide1 from '../../images/main-page/slide_img-1.jpg';
+import slide2 from '../../images/main-page/slide_img-2.jpg';
+import slide3 from '../../images/main-page/slide_img-3.jpg';
+
 function Slider() {
   return (
   <React.Fragment>
   <div id="carouselExampleInterval" class="carousel slide" data-ride="carousel">
     <div class="carousel-inner">
-      <div class="carousel-item active" data-interval="500">
-        <img src="" class="d-block w-100" alt="..."></img>
+      <div class="carousel-item active" data-interval="100">
+        <img src={slide1} class="d-block w-100" alt="123"></img>
       </div>
-      <div class="carousel-item" data-interval="500">
-        <img src="" class="d-block w-100" alt="..."></img>
+      <div class="carousel-item" data-interval="100">
+        <img src={slide2} class="d-block w-100" alt="228"></img>
       </div>
-      <div class="carousel-item" data-interval="500">
-        <img src="" class="d-block w-100" alt="..."></img>
+      <div class="carousel-item" data-interval="100">
+        <img src={slide3} class="d-block w-100" alt="322"></img>
       </div>
     </div>
     <a class="carousel-control-prev" href="#carouselExampleInterval" role="button" data-slide="prev">
