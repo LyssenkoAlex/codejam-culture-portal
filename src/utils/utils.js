5df9bdb8c2c0efca75d4542070ef6f575c73ac6d
Jimmba <gimvit@gmail.com>
2020-02-26 22:30:14 +0300

    add path for author component, add component daysAuthor, add directors description

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -6,8 +6,10 @@ export const WORK_LOG = {ID: 'WORK_LOG', ru: 'Журнал', en: 'Work log', by:
 export const THEATER_TITLE = {ID:'Theater directors of Belarus', ru:'Театральные режиссеры Беларуси', en:'Theater directors of Belarus', by:'Тэатральныя рэжысёры Беларусі'};
 export const WORKS = {ID:'WORKS', ru:'Работы', en:'Works', by:'Работы'};
 export const DETAILS = {ID:'DETAILS', ru:'Детали', en:'Details', by:'Дэталi'};
-export const BIOGRAPHY = {ID: 'BIOGRAPHY', ru:'Биография', en: 'Biography', by: 'Біяграфія'}
-export const DIRECTOR_SEARCH = {ID: 'DIRECTORSEARCH', ru:'Поиск режиссера', en: 'Director search', by: 'Пошук рэжыссера'}
+export const BIOGRAPHY = {ID: 'BIOGRAPHY', ru:'Биография', en: 'Biography', by: 'Біяграфія'};
+export const DIRECTOR_SEARCH = {ID: 'DIRECTORSEARCH', ru:'Поиск режиссера', en: 'Director search', by: 'Пошук рэжыссера'};
+export const DAYSAUTHOR = {ID: 'DAYSAUTHOR', ru:'Режиссер дня', en: 'Director of the day', by: 'Рэжысёр дня', path: '/daysAuthor'};
+export const AUTHOR = {ID: 'AUTHOR', path: '/author'};
 
 export const NAV_PAGES = [HOME, DIRECTORS, TEAM, STYLE, WORK_LOG];
 
