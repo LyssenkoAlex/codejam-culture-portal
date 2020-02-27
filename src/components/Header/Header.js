96f1250220a3dfc50afcc449f2edb2c61ec5a0df
Nij4life <nij4life@gmail.com>
2020-02-27 14:57:20 +0300

    fix: fix menu of language

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import {HOME, DIRECTORS, TEAM, WORK_LOG, LANG} from '../../utils/utils';
-import {changeLanguage} from '../../redux/actions/actions';
+import { HOME, DIRECTORS, TEAM, WORK_LOG, LANG } from '../../utils/utils';
+import { changeLanguage } from '../../redux/actions/actions';
 import BottomNavigation from '@material-ui/core/BottomNavigation';
 import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
 import HomeIcon from '@material-ui/icons/Home';
@@ -9,7 +9,7 @@ import PeopleIcon from '@material-ui/icons/People';
 import FitnessCenterIcon from '@material-ui/icons/FitnessCenter';
 import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
-import {useDispatch, useSelector} from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import Button from '@material-ui/core/Button';
 import { Link } from 'react-router-dom';
 
@@ -20,9 +20,9 @@ export default function Header() {
         setAnchorEl(event.currentTarget);
     };
 
-    const handleClose = (id) => {
+    const handleClose = (e, id) => {
         setAnchorEl(null);
-        dispatch(changeLanguage(id));
+        if (e.target.tagName === 'LI') dispatch(changeLanguage(id));
     };
 
     const language = useSelector(state => state.language);
@@ -31,10 +31,10 @@ export default function Header() {
     return (
         <div>
             <BottomNavigation showLabels className='header_props'>
-                <BottomNavigationAction label={HOME[language]} icon={<HomeIcon/>} component={Link} to={HOME.path}/>
-                <BottomNavigationAction label={DIRECTORS[language]} icon={<SupervisorAccountIcon/>} component={Link} to={DIRECTORS.path}/>
-                <BottomNavigationAction label={TEAM[language]} icon={<PeopleIcon/>} component={Link} to={TEAM.path}/>
-                <BottomNavigationAction label={WORK_LOG[language]} icon={<FitnessCenterIcon/>} component={Link} to={WORK_LOG.path}/>
+                <BottomNavigationAction label={HOME[language]} icon={<HomeIcon />} component={Link} to={HOME.path} />
+                <BottomNavigationAction label={DIRECTORS[language]} icon={<SupervisorAccountIcon />} component={Link} to={DIRECTORS.path} />
+                <BottomNavigationAction label={TEAM[language]} icon={<PeopleIcon />} component={Link} to={TEAM.path} />
+                <BottomNavigationAction label={WORK_LOG[language]} icon={<FitnessCenterIcon />} component={Link} to={WORK_LOG.path} />
                 <Button aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
                     {language.toUpperCase()}
                 </Button>
@@ -44,9 +44,9 @@ export default function Header() {
                     keepMounted
                     open={Boolean(anchorEl)}
                     onClose={handleClose}>
-                    <MenuItem onClick={() => handleClose('RU')}>{LANG.RU.TITLE}</MenuItem>
-                    <MenuItem onClick={() => handleClose('BY')}>{LANG.BY.TITLE}</MenuItem>
-                    <MenuItem onClick={() => handleClose('ENG')}>{LANG.ENG.TITLE}</MenuItem>
+                    <MenuItem onClick={(e) => handleClose(e, 'RU')}>{LANG.RU.TITLE}</MenuItem>
+                    <MenuItem onClick={(e) => handleClose(e, 'BY')}>{LANG.BY.TITLE}</MenuItem>
+                    <MenuItem onClick={(e) => handleClose(e, 'ENG')}>{LANG.ENG.TITLE}</MenuItem>
                 </Menu>
             </BottomNavigation>
         </div>
